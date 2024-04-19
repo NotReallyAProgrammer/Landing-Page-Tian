fcc2bd7672f3c43016c0044a5bc7d56b2249299e
NotReallyAProgrammer <73622276+NotReallyAProgrammer@users.noreply.github.com>
2024-04-19 21:32:58 +0800

    footer mobile

--- src/app/pages/landin-page/landin-page.component.ts
+++ src/app/pages/landin-page/landin-page.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../../layouts/header/header.component';
+import { FooterComponent } from '../../layouts/footer/footer.component';
 
 @Component({
   selector: 'app-landin-page',
   standalone: true,
   templateUrl: './landin-page.component.html',
   styleUrl: './landin-page.component.scss',
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, FooterComponent],
 })
 export class LandinPageComponent {}
