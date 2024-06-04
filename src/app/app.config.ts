8c39cef352279e5dbede33f8aa546a28b4f9e0ea
NotReallyAProgrammer <73622276+NotReallyAProgrammer@users.noreply.github.com>
2024-06-04 20:24:25 +0800

    icon mode

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,8 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
+import { BrowserModule } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration()]
+  providers: [provideRouter(routes), provideClientHydration()],
 };
